c862f6701a975df70c725ecaca6791c3d0862d09
vernaboutiqueshop-ui <vernaboutiqueshop@gmail.com>
2026-05-02 15:27:50 -0300

    fix: sync with tunnel members-drive-justin-treat

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,4 +1,4 @@
-const currentAPI = 'https://minutes-organisation-agree-proportion.trycloudflare.com';
+const currentAPI = 'https://members-drive-justin-treat.trycloudflare.com';
 
 export const API = currentAPI;
 export default API;
