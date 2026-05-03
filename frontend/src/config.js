4a74f6d0a7dac30da6573737bea744215b0609ec
vernaboutiqueshop-ui <vernaboutiqueshop@gmail.com>
2026-05-02 22:44:44 -0300

    fix: actualizar URL del backend a nuevo tunnel Cloudflare

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,4 +1,4 @@
-const currentAPI = 'https://belly-iowa-titten-batteries.trycloudflare.com';
+const currentAPI = 'https://incomplete-modifications-defence-dakota.trycloudflare.com';
 
 export const API = currentAPI;
 export default API;
